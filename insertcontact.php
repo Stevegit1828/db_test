21eff153dbb02fefac527b3e293c336c1be48d95
Stevegit1828 <Steve.a.lewis@icloud.com>
2023-01-09 19:22:00 +0000

    first database modules

---
+++ insertcontact.php
@@ -0,0 +1,49 @@
+<?php
+// database connection code
+if(isset($_POST['txtName']))
+{
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+$con = mysqli_connect('localhost', 'root', '','db_contact');
+
+// get the post records
+
+$txtName = $_POST['txtName'];
+$txtEmail = $_POST['txtEmail'];
+$txtPhone = $_POST['txtPhone'];
+$txtMessage = $_POST['txtMessage'];
+
+   $host_name = 'db5011484238.hosting-data.io';
+  $database = 'dbs9688764';
+  $user_name = 'dbu5106760';
+  $password = 'Ihnsasp0tted0tter';
+
+  $con = new mysqli($host_name, $user_name, $password, $database);
+
+  if ($link->connect_error) {
+    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
+  } else {
+    echo '<p>Connection to MySQL server successfully established.</p>';
+  } 
+  
+// database insert SQL code
+$sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+if($rs)
+{
+	echo "Contact Records Inserted";
+}
+}
+else
+{
+	echo "Are you a genuine visitor?";
+	
+}
+?>
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+
